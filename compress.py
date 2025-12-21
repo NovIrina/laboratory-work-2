033bcce52fc8e9dd23d5fbe6749f7fc5c40d90a5
NovIrina <zahara.novikova2014@yandex.ru>
2025-12-21 15:21:33 +0300

    laboratory work #2

---
+++ compress.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from tap import Tap
+
+from transformers import AutoModelForCausalLM, BitsAndBytesConfig
+
+
+class CommandLineArguments(Tap):
+    path_to_model: Path
+    path_to_save: Path
+
+
+def compress_model(path_to_model: Path):
+    quantization_config = BitsAndBytesConfig(load_in_4bit=True)
+    model = AutoModelForCausalLM.from_pretrained(
+        path_to_model,
+        torch_dtype="auto",
+        quantization_config=quantization_config
+    )
+    return model
+
+
+def main(arguments: CommandLineArguments):
+    model = compress_model(arguments.path_to_model)
+    model.save_pretrained(arguments.path_to_save)
+
+
+if __name__ == "__main__":
+    ARGUMENTS = CommandLineArguments(underscores_to_dashed=True).parse_args()
+    main(ARGUMENTS)
---
+++ evaluation.py
@@ -0,0 +1,88 @@
+from pathlib import Path
+from tap import Tap
+
+import lm_eval
+from lm_eval.api.registry import get_model
+from lm_eval.models.huggingface import HFLM
+
+
+class CommandLineArguments(Tap):
+    path_to_original_model: Path
+    path_to_compressed_model: Path
+    batch_size: int
+
+
+def load_models(path_to_original_model: Path, path_to_compressed_model: Path, batch_size: int) -> tuple[HFLM, HFLM]:
+    original_model = get_model("hf").create_from_arg_string(
+        f"pretrained={path_to_original_model},device=cuda,parallelize=True,trust_remote_code=True",
+        additional_config={
+            "batch_size": batch_size,
+            "max_batch_size": batch_size
+        }
+    )
+
+    compressed_model = get_model("hf").create_from_arg_string(
+        f"pretrained={path_to_compressed_model},device=cuda,parallelize=True,trust_remote_code=True",
+        additional_config={
+            "batch_size": batch_size,
+            "max_batch_size": batch_size
+        }
+    )
+
+    return original_model, compressed_model
+
+
+def check_memory_footprint(original_model: HFLM, compressed_model: HFLM) -> None:
+    original_model_memory_footprint = original_model.model.get_memory_footprint()
+    compressed_model_memory_footprint = compressed_model.model.get_memory_footprint()
+
+    compression_ratio = original_model_memory_footprint / compressed_model_memory_footprint
+    print(f"Compression Ratio: {compression_ratio}")
+    return compression_ratio
+
+
+def check_quality(path_to_original_model: Path, path_to_compressed_model: Path, original_model: HFLM, compressed_model: HFLM, batch_size: int) -> None:
+    original_results = lm_eval.simple_evaluate(
+        model=original_model,
+        model_args=f"pretrained={path_to_original_model}",
+        tasks="mmlu",
+        batch_size=batch_size,
+        device="cuda",
+        num_fewshot=0,
+    )["results"]["mmlu"]["acc,none"]
+
+    compressed_results = lm_eval.simple_evaluate(
+        model=compressed_model,
+        model_args=f"pretrained={path_to_compressed_model}",
+        tasks="mmlu",
+        batch_size=batch_size,
+        device="cuda",
+        num_fewshot=0,
+    )["results"]["mmlu"]["acc,none"]
+
+    performance_drop = (original_results - compressed_results) / original_results
+    return performance_drop
+
+
+def get_score(compression_ratio: float, performance_drop: float) -> float:
+    score = compression_ratio / (1 + performance_drop)
+    return score
+
+
+def print_report(compression_ratio: float, performance_drop: float, score: float) -> None:
+    print(f"Compression Ratio: {compression_ratio}")
+    print(f"Performance Drop: {performance_drop}")
+    print(f"Score: {score}")
+
+
+def main(arguments: CommandLineArguments):
+    original_model, compressed_model = load_models(arguments.path_to_original_model, arguments.path_to_compressed_model, arguments.batch_size)
+    compression_ratio = check_memory_footprint(original_model, compressed_model)
+    performnace_drop = check_quality(original_model, compressed_model)
+    score = get_score(compression_ratio, performnace_drop)
+    print_report(compression_ratio, performnace_drop, score)
+
+
+if __name__ == "__main__":
+    ARGUMENTS = CommandLineArguments(underscores_to_dashed=True).parse_args()
+    main(ARGUMENTS)
